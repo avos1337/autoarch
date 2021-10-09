2b5c004488032f1288f1e7f52343a94a7448392d
avos1337 <avos1337@hotmail.com>
2021-10-09 17:02:21 +0200

    debug 2

--- config.sh
+++ config.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Set nonroot user to do most of the script in
-echo What is the non-root user that will be used to complete the installation?\n
+echo What is the non-root user that will be used to complete the installation?
 read nonrootusr
 export nonrootusr
 
 # Set weather to use custom .vimrc
-echo "Should autoarch setup a custom vimrc? (Press enter for default custom vimrc)\n"
+echo "Should autoarch setup a custom vimrc? (Press enter for default custom vimrc)"
 read docustvimrc
 
-if [ $docustvimrc == '' ]; then
-    export docustvimrc='true'
-    export custvimrcpath='default'
+if [[ $docustvimrc == "" ]]; then
+    export docustvimrc="true"
+    export custvimrcpath="default"
 else
-    if [[ $docustvimrc == 'yes' || $docustvimrc == 'y' || $docustvimrc == 'Y' ]]; then
+    if [[ $docustvimrc == "yes"  ||  $docustvimrc == "y"  ||  $docustvimrc == "Y" ]]; then
         export docustvimrc='true'
+        echo "Please input the custum vimrc url or path"
         read custvimrcpath
         export custvimrcpath
     else
