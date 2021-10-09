ef65d2fd8c784194cfe7dd7fc5d777acceec23a3
avos1337 <avos1337@hotmail.com>
2021-10-09 14:54:58 +0200

    Began and ended autoarch.sh. Now does full sys upgrade and reboots

---
+++ autoarch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dank arch machine setup starts here
+sudo pacman -Syu --noconfirm    # Update all packages
+
+
+
+
+
+
+
+# Dank arch machine setup ends here
+sudo pacman -Syu --noconfirm    # Update all packages
+
+sudo reboot     #Reboot dank machine
